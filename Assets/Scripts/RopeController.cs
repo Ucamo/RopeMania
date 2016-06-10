35a106a5a0bc9bf008adb2845951bb3206457a42
Uriel Carrillo <carrillouriel@gmail.com>
2016-06-10 00:59:01 -0700

    The rope works

---
+++ Assets/Scripts/RopeController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RopeController : MonoBehaviour {
+
+
+	string layerName="Front";
+
+
+	void Start () {
+		InvokeRepeating("HandleRopeMovement", 0.5f, 1.3f);
+		InvokeRepeating("ChangeLayer", 0.5f, 0.65f);
+
+	}
+
+	void Update () {
+		
+	}
+
+	void HandleRopeMovement()
+	{ 
+		
+		GameObject[] movingRopes = GameObject.FindGameObjectsWithTag("MovingRope");
+		foreach (GameObject ropes in movingRopes) {
+
+			ropes.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, 900));
+		}
+				
+	}
+
+	void ChangeLayer()
+	{
+		
+		if (layerName == "Front")
+			layerName = "Back";
+		else
+			layerName="Front";
+
+		GameObject[] movingRopes = GameObject.FindGameObjectsWithTag("MovingRope");
+		foreach (GameObject ropes in movingRopes) {
+
+			SpriteRenderer sprite = ropes.GetComponent<SpriteRenderer>();
+
+			if (sprite)
+			{
+				sprite.sortingOrder = 1;
+				sprite.sortingLayerName = layerName;
+			}
+				
+		}
+
+	}
+
+
+}
---
+++ Assets/Scripts/TestRope.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestRope : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Quaternion.Euler(60, 0, 0) * Vector3.left, -240 * Time.deltaTime);
+	}
+}
