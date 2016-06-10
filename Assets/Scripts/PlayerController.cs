c6858daf5e3aa1303f17afa471d31d7a56e86fa7
Uriel Carrillo <carrillouriel@gmail.com>
2016-06-09 19:10:39 -0700

    Base Project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float jumpForce;
+	public bool grounded;
+	public bool candoublejump;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HandleInput ();
+	}
+
+	void HandleInput()
+	{
+		
+		if (Input.GetMouseButtonDown(0))
+		{
+			Jump();
+		}
+
+
+
+
+	}
+
+	//Handle ground contact
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		//Contact with ground
+		if (coll.gameObject.tag == "Ground")
+		{
+			grounded = true;
+		}
+	}
+
+
+	void Jump()
+	{
+		if (grounded) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2(GetComponent<Rigidbody2D> ().velocity.x, 0);
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, jumpForce));
+			candoublejump = true;
+			grounded = false;
+		}
+	}
+}
