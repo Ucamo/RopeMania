f9a2107bee1f9c1bc6eeb8ab6cd5e599cedb2f0a
Uriel Carrillo <carrillouriel@gmail.com>
2016-06-18 22:23:19 -0700

    Finished implementing GameJolt Score System

--- Assets/Scripts/HighScoreSystem.cs
+++ Assets/Scripts/HighScoreSystem.cs
@@ -10,12 +10,14 @@ public class HighScoreSystem : MonoBehaviour {
 	int highScore;
 	bool writeOnScoreBoard=false;
 	int lastHighScore;
+	int achievedTrophyId;
 
 	void Start () {
 
 		thePlayer = GameObject.Find("Player");
 		objPlayer = thePlayer.GetComponent<PlayerController>();
 		highScore=PlayerPrefs.GetInt("highScore", highScore);
+		achievedTrophyId = PlayerPrefs.GetInt ("60315", achievedTrophyId);
 	
 	}
 	
@@ -23,6 +25,7 @@ public class HighScoreSystem : MonoBehaviour {
 	void Update () {
 		score = objPlayer.getScore ();
 		ScoreHandler ();
+		TrophyHandler ();
 	}
 
 	void ScoreHandler()
@@ -34,6 +37,74 @@ public class HighScoreSystem : MonoBehaviour {
 			}
 		}
 	
+	}
+
+	void UnlockTrophy(int idTrophy)
+	{
+		achievedTrophyId=PlayerPrefs.GetInt (idTrophy.ToString(), achievedTrophyId);
+		if (achievedTrophyId != idTrophy) {
+			GameJolt.API.Trophies.Unlock (idTrophy);
+			PlayerPrefs.SetInt (idTrophy.ToString(), idTrophy);
+		}
+	}
+
+	void TrophyHandler()
+	{
+		int idTrophy = 0;
+		if (!objPlayer.getGameOver ()) {
+			if (score >= 10) {
+				idTrophy = 60315;
+				UnlockTrophy (idTrophy);
+			}
+			if (score >= 20) {
+				idTrophy = 60316;
+				UnlockTrophy (idTrophy);
+			}
+			if (score >= 30) {
+				idTrophy = 60317;
+				UnlockTrophy (idTrophy);
+			}
+			if (score >= 50) {
+				idTrophy = 60318;
+				UnlockTrophy (idTrophy);
+			}
+			if (score >= 60) {
+				idTrophy = 60319;
+				UnlockTrophy (idTrophy);
+			}
+			if (score >= 70) {
+				idTrophy = 60320;
+				UnlockTrophy (idTrophy);
+			}
+			if (score >= 100) {
+				idTrophy = 60322;
+				UnlockTrophy (idTrophy);
+			}
+			if (score >= 120) {
+				idTrophy = 60323;
+				UnlockTrophy (idTrophy);
+			}
+			if (score >= 130) {
+				idTrophy = 60324;
+				UnlockTrophy (idTrophy);
+			}
+			if (score >= 150) {
+				idTrophy = 60325;
+				UnlockTrophy (idTrophy);
+			}
+			if (score >= 200) {
+				idTrophy = 60326;
+				UnlockTrophy (idTrophy);
+			}
+			if (score >= 250) {
+				idTrophy = 60327;
+				UnlockTrophy (idTrophy);
+			}
+			if (score >= 300) {
+				idTrophy = 60328;
+				UnlockTrophy (idTrophy);
+			}
+		}
 	}
 
 	void SetHighScore(int score)
