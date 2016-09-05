db4b4b769285b0c9b525603670032ae17d3fac35
Uriel Carrillo <carrillouriel@gmail.com>
2016-09-04 23:18:56 -0700

    Added the dog prefab and animation as a wip on level1

---
+++ Assets/Scripts/DogController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DogController : MonoBehaviour {
+
+	float movementSpeed=10f;
+	void Start () {
+	
+	}
+
+	void Update () {
+		transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+	}
+}
