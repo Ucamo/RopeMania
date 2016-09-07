be1ce042e57205e17a621fb64a8774ef1e93663f
Uriel Carrillo <carrillouriel@gmail.com>
2016-09-07 00:37:27 -0700

    The dog jumps with the player, but sometimes the dog spawner stops spawning dogs, see what is going on

--- Assets/Scripts/DogController.cs
+++ Assets/Scripts/DogController.cs
@@ -8,15 +8,19 @@ public class DogController : MonoBehaviour {
 	public float jumpForce;
 
 	float movementSpeed=3f;
-	Vector3 rightFromPlayer = new Vector3(3f,-2.8f);
+	Vector3 rightFromPlayer = new Vector3(2.5f,-2.8f);
+	Vector3 awayToTheRight = new  Vector3(4f,-2.8f);
 	int jumps = 0;
 	GameObject thePlayer;
 	PlayerController objPlayer;
+	bool isOnRightFromPlayer=false;
+	int counter=0;
+	bool moveAway=false;
 
 
 	void Start () {
 		//Dog will jump a random number of times between 1 and 10
-		jumps = Random.Range (1, 11);
+		jumps = Random.Range (2, 11);
 		thePlayer = GameObject.Find("Player");
 		objPlayer = thePlayer.GetComponent<PlayerController>();
 
@@ -33,14 +37,27 @@ public class DogController : MonoBehaviour {
 		//Move towards the right spot from the player
 		float time = Time.deltaTime*movementSpeed;
 		Vector3 currentPosition = transform.position;
-		transform.position = Vector3.MoveTowards (transform.position, rightFromPlayer, time);
+		if(!isOnRightFromPlayer)
+			transform.position = Vector3.MoveTowards (transform.position, rightFromPlayer, time);
 
 		if (currentPosition == rightFromPlayer) {
+			isOnRightFromPlayer = true;
+		}
+
+		if (isOnRightFromPlayer) {
 			//Stop walking, start jumping.
 			SwitchIdle();
 			//count how many jumps the dog will jump
-			Jump ();
-
+			if (counter < jumps) {
+				Jump ();
+				counter++;
+			} else {
+				moveAway = true;
+			}
+		}
+		if (moveAway) {
+			//GetComponent<Animator> ().Play ("dog_walk_1");
+			transform.position = Vector3.MoveTowards (transform.position, awayToTheRight, time);
 		}
 	}
 		
